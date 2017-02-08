f9b40080da498f68de9343c285c2afc9c77a5bc7
xiayuanqun <xiayuanqun@xiayuanqundemacbook-pro.local>
2017-02-08 19:25:41 +0800

    Initial Commit

---
+++ 类似于广告的图片轮播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  类似于广告的图片轮播
+//
+//  Created by xiayuanqun on 17/2/8.
+//  Copyright © 2017年 xiayuanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 类似于广告的图片轮播/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  类似于广告的图片轮播
+//
+//  Created by xiayuanqun on 17/2/8.
+//  Copyright © 2017年 xiayuanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
