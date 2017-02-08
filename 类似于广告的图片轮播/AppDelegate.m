fe6e6fd8c3809802f4b808120470ce3d69d920b1
xiayuanqun <xiayuanqun@xiayuanqundeMacBook-Pro.local>
2017-02-08 20:38:39 +0800

    first

--- 类似于广告的图片轮播/AppDelegate.m
+++ 类似于广告的图片轮播/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,11 +16,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    //创建窗口 设置frame
+  //  self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    //设置窗口的颜色
+ //   self.window.backgroundColor = [UIColor whiteColor];
+    //设置根控制器
+ //   ViewController *vc = [[ViewController alloc]init];
+    //显示窗口
+//    [self.window makeKeyAndVisible];
     return YES;
 }
-
-
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
